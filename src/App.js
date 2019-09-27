a28c8275d2fd80ea14e787f6433d44114d133e1f
doorisopen <audauddl2@gmail.com>
2019-09-27 11:51:29 +0900

    #0.3 Movie App done :)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,58 @@
-import React from 'react';
-
+import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   state = {
-    isLoading: true
+    isLoading: true,
+    movies: []
   };
-  // component가 생성되고 호출되는 life Cycle
+  
+
+  // async: 이 함수가 비동기라는것을 말해주는것이고 await은 axios로 fetch 해온 data를 모두 로드 할때 까지 기다리라는 말임
+  getMovies = async() => {
+    const {
+      data : {
+         data: { movies }
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    // state-> movies:movies <- axios에서 가져온 moives임
+    this.setState({ movies, isLoading: false });
+  }; 
+
+  // componentDidMount: component가 생성되고 호출되는 life Cycle
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+    this.getMovies();
   }
+
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "We are ready"}</div>
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+           ) : (
+            <div className="movies">
+              {movies.map(movie => (
+                <Movie 
+                  key={movie.id}
+                  id={movie.id} 
+                  year={movie.year} 
+                  title={movie.title} 
+                  summary={movie.summary} 
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                />
+              ))}
+            </div>
+          )}
+      </section>
+    );
   }
 }
 
